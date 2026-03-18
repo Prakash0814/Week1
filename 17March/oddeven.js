5b2a385b2b82385218555d3bb839837b85be3f1e
Prakash0814 <prakaashsun@gmail.com>
2026-03-18 19:31:11 +0530

    oddeven.js

---
+++ 17March/oddeven.js
@@ -0,0 +1,14 @@
+
+//Creating Function
+function OddorEven (){
+    let num = 11;
+
+    //Trying with Condition Statement
+if (num %2==0){
+    console.log("Even");
+    }
+        else {
+            console.log("odd");  
+        }
+    }
+    OddorEven()
