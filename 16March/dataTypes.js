4ad9323d318b42c675795d9fa205a011f7d1536e
Prakash0814 <prakaashsun@gmail.com>
2026-03-18 19:24:39 +0530

    dataTypes.js

---
+++ 16March/dataTypes.js
@@ -0,0 +1,23 @@
+let userName="JIJO"
+console.log(userName);
+console.log(typeof userName);
+
+
+let contactnumber=136
+console.log(contactnumber);
+console.log((typeof contactnumber));
+
+let oraganization="Testleaf"
+console.log(oraganization);
+console.log(typeof oraganization);
+
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let userPlaywright=null
+console.log(userPlaywright);
+console.log(typeof userPlaywright);
+
+var a=10
+console.log(a);
